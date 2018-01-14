5b67a0adc6ce5778df03bb201deb23b694111fc0
Maxim <maxim.akristiniy@gmail.com>
2018-01-14 20:59:36 +0300

    fixed error in load tabs

--- restoretabs.py
+++ restoretabs.py
@@ -1,6 +1,7 @@
 import os
 import logging, sys
 import threading
+import time
 from gi.repository import GObject, GLib, Gtk, Gio, Gedit
 
 SETTINGS_SCHEMA = "org.gnome.gedit.plugins.restoretabs"
@@ -25,7 +26,7 @@ class RestoreTabsWindowActivatable(GObject.Object, Gedit.WindowActivatable):
         """
         Connect signal handlers.
         """
-        logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+        logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
 
         handlers = []
         handler_id = self.window.connect("delete-event", self.on_window_delete_event)
@@ -46,7 +47,7 @@ class RestoreTabsWindowActivatable(GObject.Object, Gedit.WindowActivatable):
                 if uri not in RestoreTabsWindowActivatable.uris:
                     RestoreTabsWindowActivatable.uris.append(uri)
             # temporary handler to catch the first time a window is shown
-            self._temp_handler = self.window.connect("show", self.on_window_show, RestoreTabsWindowActivatable.uris)
+            self._temp_handler = self.window.connect("show", self.on_window_show, RestoreTabsWindowActivatable.uris[:])
             RestoreTabsWindowActivatable.lock.release()
 
     def do_deactivate(self):
@@ -63,10 +64,13 @@ class RestoreTabsWindowActivatable(GObject.Object, Gedit.WindowActivatable):
 
         RestoreTabsWindowActivatable.lock.acquire()
         if code not in RestoreTabsWindowActivatable.uris:
-            logging.debug("tab added "+code)
+            logging.debug("uri added "+code)
             RestoreTabsWindowActivatable.uris.append(code)
             settings = Gio.Settings.new(SETTINGS_SCHEMA)
             settings.set_value('uris', GLib.Variant("as", RestoreTabsWindowActivatable.uris))
+        else:
+            logging.debug("uri already in list, ignoring "+code)
+
         RestoreTabsWindowActivatable.lock.release()
 
     def remove_uri(self,uri, raw_data=False):
@@ -81,10 +85,12 @@ class RestoreTabsWindowActivatable(GObject.Object, Gedit.WindowActivatable):
 
         RestoreTabsWindowActivatable.lock.acquire()
         if code in RestoreTabsWindowActivatable.uris:
-            logging.debug("tab removed "+code)
+            logging.debug("uri removed: "+code+" ,raw_data="+str(raw_data))
             RestoreTabsWindowActivatable.uris.remove(code)
             settings = Gio.Settings.new(SETTINGS_SCHEMA)
             settings.set_value('uris', GLib.Variant("as", RestoreTabsWindowActivatable.uris))
+        else:
+            logging.debug("uri not in list, ignoring "+code)
         RestoreTabsWindowActivatable.lock.release()
 
     def on_window_tab_added(self, window, tab):
@@ -95,7 +101,7 @@ class RestoreTabsWindowActivatable(GObject.Object, Gedit.WindowActivatable):
 
         gfile = tab.get_document().get_location()
         if gfile:
-            logging.debug ("gfile not null")
+            logging.debug ("gfile not null, processing to add uri")
             self.add_uri(gfile.get_uri())
 
     def on_window_tab_removed(self, window, tab):
@@ -103,13 +109,13 @@ class RestoreTabsWindowActivatable(GObject.Object, Gedit.WindowActivatable):
         remove tab from list
         """
         logging.debug("on_window_tab_removed")
-        if tab.get_state() == Gedit.TabState.STATE_NORMAL and tab.get_document().is_untouched():
+        if tab.get_state() == Gedit.TabState.STATE_NORMAL and tab.get_document().is_untouched() and tab.get_document().get_location() == None:
             logging.debug("tab untouched, skipping")
             return
 
         gfile = tab.get_document().get_location()
         if gfile:
-            logging.debug ("gfile not null")
+            logging.debug ("gfile not null, processing to delete uri")
             self.remove_uri(gfile.get_uri())
 
     def on_window_delete_event(self, window, event, data=None):
@@ -148,21 +154,30 @@ class RestoreTabsWindowActivatable(GObject.Object, Gedit.WindowActivatable):
         if active_tab:
             self.on_tab_added(window, active_tab)
 
+        logging.debug ("tatal processing tabs records: "+str(len(uris)))
+
         for uri in uris:
-            logging.debug ("restoring "+uri)
+            logging.debug ("restoring: "+uri)
             try:
-                num_str,uri_str = uri.split(RestoreTabsWindowActivatable._split_symbol)
+                num_str,uri_str = uri.split(self._split_symbol)
             except ValueError:
-                logging.error("wrong format of saved string "+uri)
+                logging.error("wrong format of saved string, possible another plugin format: "+uri)
+                #wrong format, possible from another plugin                
+                uri_str = uri
+                num_str = str(self._curr_instance)
+                self.remove_uri(uri,True)
+            except Exception:
+                logging.error("wrong format of saved string, ignoring: "+uri)
                 self.remove_uri(uri,True)
                 continue
 
             if self._curr_instance != int(num_str):
-                logging.debug ("not current instance, ignoring "+num_str)
+                logging.debug ("not current instance, ignoring: "+num_str)
                 continue
 
             if not os.path.isfile(uri_str if uri_str[:7]!='file://' else uri_str[7:]):
-                logging.debug ("file not exist, removing from list "+uri_str)
+                logging.error ("file not exist, removing from list "+uri_str)
+                self.remove_uri(uri,True)
                 continue
 
             location = Gio.file_new_for_uri(uri_str)
@@ -180,8 +195,9 @@ class RestoreTabsWindowActivatable(GObject.Object, Gedit.WindowActivatable):
         """
         logging.debug ("on_tab_added?")
 
-        if tab.get_state() == Gedit.TabState.STATE_NORMAL and tab.get_document().is_untouched():
-            def close_tab():
+        if tab.get_state() == Gedit.TabState.STATE_NORMAL and tab.get_document().is_untouched() and tab.get_document().get_location() == None:
+            def close_tab():    
+                logging.debug ("closing empty tab")           
                 window.close_tab(tab)
                 return False
             try:
